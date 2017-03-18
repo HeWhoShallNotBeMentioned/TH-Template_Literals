0ad49cd0ff4e478294ad15d87dca92d7f727b68e
Chris Underwood <cunderwoodmn@gmail.com>
2017-03-17 19:19:39 -0700

    Add multiline template literal

--- multiline.js
+++ multiline.js
@@ -1,8 +1,17 @@
-const fruitList = 
+const fruitList =
   "<ul>" +
     "<li>Kiwi</li>" +
     "<li>Lime</li>" +
     "<li>Pineapple</li>" +
   "</ul>";
 
-//document.querySelector('.fruits').innerHTML = fruitList;
+document.querySelector('.fruits').innerHTML = fruitList;
+
+const vegList =
+  `<ul>
+    <li>Pickle</li>
+    <li>Corn</li>
+    <li>Cucumber</li>
+  </ul>`;
+
+document.querySelector('.vegetables').innerHTML = vegList;
