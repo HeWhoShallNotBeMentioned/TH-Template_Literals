ac7e22a0bf0524ebd49249fcbdeb1d8d72afcdf2
Chris Underwood <cunderwoodmn@gmail.com>
2017-03-17 17:35:36 -0700

    Created first template literal and showed it on the screen.

--- basic.js
+++ basic.js
@@ -1,5 +1,6 @@
 const singleQuotes = '<p>Single quotes</p>';
 const doubleQuotes = "<p>Double quotes</p>";
+const tempLit = `<p>Template Literal</p>`;
 
-const result = singleQuotes + doubleQuotes;
-//document.querySelector('.basic').innerHTML = result;
+const result = singleQuotes + doubleQuotes + tempLit;
+document.querySelector('.basic').innerHTML = result;
